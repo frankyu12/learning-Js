671c81b8e0757f4d89a173696ead6e60a508b022
frankyu12 <75060875+frankyu12@users.noreply.github.com>
2021-06-24 04:58:45 +0900

    chap7

--- 2/app.js
+++ 2/app.js
@@ -135,9 +135,70 @@ bgImage.src = `${chosenImage}`;
 document.body.appendChild(bgImage)
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//toDo.js
+
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = toDoForm.querySelector('input');
+const toDoList = document.querySelector('#todo-list');
+
+const TODOS_KEY = "todos";
+
+//업데이트를 하기 위함
+let toDos = [];
+
+//저장하기  localStorage에 !!
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY , JSON.stringify(toDos)); //string형태로 불러와야함
+}
+
+//todo삭제버튼 함수 삭제 원리는 삭제하고자하는 객체의 아이디를 추출함
+function deleteTodo(event){
+    const li = event.target.parentElement; //버튼의 부모를 찾아가는 방법으로
+    li.remove(); //삭제하기
+    toDos = toDos.filter(toDO => toDO.id !== parseInt(li.id));//filter함수를 이용 true면 배열에 남겨둔다 거짓이면 배열에서 제외한다. 
+    saveToDos() //삭제하고 새로운 배열을 만들었으니 다시 저장해야 함 localStorage에 !!!!!
+}
+//화면에 보이기 위한 작업
+function paintToDo(newTodo) {
+    const li = document.createElement("li"); //li생성
+    li.id = newTodo.id;
+    const span = document.createElement("span"); //span생성
+    span.innerText = newTodo.text; //할 일 넣기
+    //삭제버튼만들기
+    const button = document.createElement("button")
+    button.innerText = "X";
+    button.addEventListener("click" , deleteTodo)
+    li.appendChild(span); //태그 안에 태그넣기
+    li.appendChild(button);    //태그 안에 태그넣기
+    toDoList.appendChild(li); //태그 안에 태그넣기
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault(); //기본값 막기
+    const newTodo =toDoInput.value; //input값 저장 새롭게 저장하는 값이다. 기존 값들은 존재함
+    toDoInput.value = ""; //빈칸으로 두기
+    const newTodoObj = { //todos에게 랜덤한 id를 줘야한다 삭제를 위해 따라서 객체로 만들어야 한다.
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+toDoForm.addEventListener("submit" , handleToDoSubmit)
 
 
+//기존의 저장된 값들을 남겨두기 위한 작업
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos){
+    const parsedToDos = JSON.parse(savedToDos); //배열형태로 만들어줘야 함
+    toDos = parsedToDos; //LET을 사용했으므로 기존의 데이터들이 날라가지 않고 업데이트됨. let의 업데이트 방식
+    parsedToDos.forEach(paintToDo);//!!!!!각각!!!!!!에다가 함수를 실행시키기 위한 방법 : 화살표 함수
+}
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
